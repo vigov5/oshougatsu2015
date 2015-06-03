8a9bea7b962f6dd44e1850e3ea20db8e2c4031cd
Nguyen Anh Tien <nguyenanhtien2210@gmail.com>
2015-06-03 18:51:05 +0700

    Add scoreboard view

--- app/common/views.py
+++ app/common/views.py
@@ -31,3 +31,27 @@ def index():
         'index.html',
         contests=contests
     )
+
+
+@app.route('/')
+@app.route('/scoreboard')
+@menu.register_menu(app, '.scoreboard', 'Score Board', order=1)
+def scoreboard():
+    users = User.query.all()
+    raw = {}
+    for user in users:
+        for submission in user.submissions:
+            if submission.is_accepted():
+                if not raw.has_key(submission.user.email):
+                    raw[submission.user.email] = 0
+                raw[submission.user.email] += submission.received_point
+
+    summary = [(email, total_score) for email, total_score in raw.items()]
+    summary = sorted(summary, key=lambda x: x[1], reverse=True)
+    max_score = summary[0][1]
+
+    return render_template(
+        'scoreboard.html',
+        max_score=max_score,
+        summary=summary
+    )
